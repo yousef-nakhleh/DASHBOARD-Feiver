09f9b0de1d7d346ab40ad5c2e508d0e92a47d314
Sef2002 <yousefnakhleh02@gmail.com>
2025-05-20 19:03:57 +0200

    Updated NewStaffModal.tsx

--- src/components/staff/NewStaffModal.tsx
+++ src/components/staff/NewStaffModal.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { supabase } from '../lib/supabase';
+import { supabase } from '../../lib/supabase';
 
 const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
