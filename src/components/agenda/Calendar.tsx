973361f4b2c382283fd88475ceb9c11bcf3a56df
Sef2002 <yousefnakhleh02@gmail.com>
2025-05-23 09:18:04 +0200

    Updated Calendar.tsx

--- src/components/agenda/Calendar.tsx
+++ src/components/agenda/Calendar.tsx
@@ -132,11 +132,11 @@ const DraggableAppointment = ({ app, onClick, flexBasis }) => {
     <div
       ref={drag}
       onClick={onClick}
-      className={`$${
+      className={`border-l-4 px-2 py-1 rounded-sm text-sm shadow-sm overflow-hidden cursor-pointer ${
+        isDragging ? 'opacity-50' : ''
+      } ${
         isPaid ? 'bg-green-100 border-green-500' : 'bg-blue-100 border-blue-500'
-      } border-l-4 px-2 py-1 rounded-sm text-sm shadow-sm overflow-hidden cursor-pointer ${{
-        isDragging: 'opacity-50',
-      }}`}
+      }`}
       style={{
         height: `${(app.duration_min / 15) * slotHeight}px`,
         flexBasis: `${flexBasis}%`,
