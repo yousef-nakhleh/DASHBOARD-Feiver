a876fc6fedd8d271423a59749bf87671967e69e8
Sef2002 <yousefnakhleh02@gmail.com>
2025-06-22 17:19:38 +0200

    Updated utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,6 @@
-// src/lib/utils.ts
-
-import { ClassValue, clsx } from "clsx";
+import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-/**
- * Utility to conditionally join class names together and resolve Tailwind conflicts.
- */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(...inputs));
 }
