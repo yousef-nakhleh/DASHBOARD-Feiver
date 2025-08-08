18ea209b282086df533c2f638c2541b20436eac6
yousef-nakhleh <sefnakhleh.ai@gmail.com>
2025-08-08 17:44:25 +0200

    Updated LoginPage.tsx

--- src/login/LoginPage.tsx
+++ src/login/LoginPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { supabase } from "@/lib/supabaseClient"
+import { supabase } from "../lib/supabaseClient"
 import { useNavigate } from "react-router-dom"
 
 export default function Login() {
