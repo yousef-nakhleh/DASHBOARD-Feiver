77d9854dd795a63f4d3ee8e6065482ec6cdc15e9
yousef-nakhleh <sefnakhleh.ai@gmail.com>
2025-08-23 18:07:12 +0200

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+// 👇 import your AuthProvider
+import { AuthProvider } from "@/context/AuthContext"; 
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <AuthProvider>          {/* <-- provide auth context here */}
+      <BrowserRouter>       {/* <-- wrap router too */}
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </StrictMode>
 );
